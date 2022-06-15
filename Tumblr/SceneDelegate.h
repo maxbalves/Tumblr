7367f11ce21fdfb44e6642e8a13074e0a725f0ac
Max Bagatini Alves <maxbagatini@hotmail.com>
2022-06-15 14:39:05 -0700

    Added prework files

---
+++ Tumblr/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  Tumblr
+//
+//  Created by Max Bagatini Alves on 6/15/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Tumblr/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Tumblr
+//
+//  Created by Max Bagatini Alves on 6/15/22.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
